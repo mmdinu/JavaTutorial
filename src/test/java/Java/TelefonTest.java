81e801246398d249e78786535ce0176dc4c0464a
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-04-06 22:33:56 +0300

    Prima ora pe Skipe

---
+++ src/test/java/Java/TelefonTest.java
@@ -0,0 +1,28 @@
+package Java;
+
+import org.junit.Test;
+
+public class TelefonTest {
+
+    @Test
+    public void TestAutomat(){
+        // vom declara mai multe obiecte de tipul Telefon (Telefon=numele clasei)
+        // obiect = instanta unei clase
+        // structura obiect = tipClasa NumeObiect=new tipClasa();
+        // daca facem un obiect care face referire la un construtor cu parametri => specific valorile
+        //parametrilor in ()
+        //putem declara n obiecte
+
+        Telefon Iphone = new Telefon("Iphone", "12", "IOS", "20pixeli");
+        Iphone.PrintMarca();
+        Iphone.PrintModel();
+
+        Telefon Samsung = new Telefon("Samsung", "J15", "Android","24pixeli");
+        Samsung.PrintMarca();
+        Samsung.PrintModel();
+        Samsung.PrintSistemOperare();
+
+
+
+    }
+}
